25cb45baa1c5bd1b1459f3d3b48fd2ef1c1cefda
zhanghui <zhanghui9700@gmail.com>
2016-10-30 14:32:04 +0800

    performance chinense nickname

--- iroman/iroman/management/commands/check_settings.py
+++ iroman/iroman/management/commands/check_settings.py
@@ -7,11 +7,13 @@ from django.conf import settings
 from django.core.management import BaseCommand
 from django.core.mail import send_mail
 
+from common import utils
+
 LOG = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
-    def _log(self, tag, result):
+    def _log(self, tag, result=True):
         label = self.style.ERROR("XXX")
         if result:
             label = self.style.MIGRATE_SUCCESS(":-)")
@@ -33,8 +35,14 @@ class Command(BaseCommand):
 
         self._log("CHECK_MAIL", result)
 
+    def _check_nick_name(self):
+        email, id_card, nickname, pwd = utils.gen_basic_info()
+        self.stdout.write("CHECK_NICKNAME email=%s, id=%s, nickname=%s" % (
+                            email, id_card, nickname)) 
+
     def handle(self, *args, **kwargs):
         self.stdout.write(__name__)
         self.stdout.write(self.style.WARNING("************CHECK START************"))
         self._check_mail()
+        self._check_nick_name()
         self.stdout.write(self.style.WARNING("************CHECK  END*************"))
