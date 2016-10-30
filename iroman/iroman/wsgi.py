aabeea3e4e6206336f71c35bf99d267afa7a812c
zhanghui <zhanghui9700@gmail.com>
2016-10-30 18:30:34 +0800

    add apache conf

--- iroman/iroman/wsgi.py
+++ iroman/iroman/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/opt/yunmall/robot/iroman')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iroman.settings")
 
 from django.core.wsgi import get_wsgi_application
